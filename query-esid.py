30e0bcec21affea09f367c5bbf56b1b05a31b7d4
Zhuo Lin <zhuolinsmile@gmail.com>
2016-10-12 16:42:58 +0800

    Update query-esid.py

--- query-esid.py
+++ query-esid.py
@@ -16,3 +16,30 @@ def query():
 
 if __name__=="__main__":
 	query()
+
+--------------------
+#1/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import sys
+import time
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch("localhost:19290")
+
+searchsql='{"fields":["appkey","msg_id","itime","msg_content"],"from":0,"size":10000,"query":{"bool":{"must":[{"term":{"appkey":"123"}},{"range":{"itime":{"gte":%d,"lte":%d}}}]}}}'
+def query(start,end):
+    sql = searchsql % (int(start),int(end))
+    #print sql
+    res = es.search(index='push_list_v1', body=sql)
+    for hit in res['hits']['hits']:
+        info = hit['fields']
+        appkey = info['appkey']
+        msgid = info['msg_id']
+        itime = info['itime']
+        msgcontent = info['msg_content']
+        print '%s\t%s\t%s\t%s' % (appkey[0], msgid[0], itime[0], msgcontent[0])
+
+if __name__=="__main__":
+    query(int(time.mktime(time.strptime(sys.argv[1],'%Y%m%d'))),int(time.mktime(time.strptime(sys.argv[2],'%Y%m%d'))))
