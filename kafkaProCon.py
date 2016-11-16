6826aed9f41cfd76ccb94d921608702c69024623
Zhuo Lin <zhuolinsmile@gmail.com>
2016-11-16 10:10:21 +0800

    Update kafkaProCon.py

--- kafkaProCon.py
+++ kafkaProCon.py
@@ -1,7 +1,7 @@
 ############################ tmp producer ###############
 import sys
 from pykafka import KafkaClient
-hosts_list = '172.16.111.111:9092'
+hosts_list = '127.0.0.1:9092'
 def produce(topic_name, value):
     client = KafkaClient(hosts=hosts_list)
     print client
@@ -19,11 +19,11 @@ if __name__=="__main__":
 ########################### tmp consumer ###################
 import json
 from pykafka import KafkaClient
-hosts = '172.16.111.111:9092'
+hosts = '127.0.0.1:9092'
 client = KafkaClient(hosts=hosts)
 #print client.topics
 #topic = client.topics["mobile_num"]
-#consumer = topic.get_balanced_consumer(consumer_group="test",zookeeper_connect='192.168.248.230:2181,192.168.248.175:2181,192.168.249.238:2181/kafka')
+#consumer = topic.get_balanced_consumer(consumer_group="test",zookeeper_connect='127.0.0.1:2181/kafka')
 #for i in range(0, 10):
 #    mes = consumer.consume()
 #    print mes.value
